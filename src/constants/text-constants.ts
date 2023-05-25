1e453a6853a371454b80ea9f5b7976dee0421f65
Alexander Gurnet <a.gurneckiy-s@sysdyn.ru>
2023-05-26 02:18:35 +0300

    added constants

---
+++ src/constants/text-constants.ts
@@ -0,0 +1,23 @@
+export const LOG_OUT_TEXT = 'Log Out';
+export const SIGN_IN_TEXT = 'Sign In';
+export const SIGN_UP_TEXT = 'Sign Up';
+export const HOME_PATH = '/';
+export const LOGO_TEXT = '🗓️ My Diary';
+
+export const GREETINGS_TEXT = 'Login to start your diary!';
+
+export const EMPTY_MSG_TEXT = "Sorry, we didn't find anything 😓";
+export const TITLE_PLACEHOLDER_TEXT = 'enter a title';
+export const DESCRIPTION_PLACEHOLDER_TEXT = 'enter a description';
+export const SEARCH_PLACEHOLDER_TEXT = 'search here...';
+
+export const TITLE_EMPTY_PLACEHOLDER_TEXT = 'title is empty';
+export const DESCRIPTION_EMPTY_PLACEHOLDER_TEXT = 'description is empty';
+export const EDIT_BTN_TEXT = 'edit';
+export const DELETE_BTN_TEXT = 'delete';
+export const CREATED_TEXT = 'created:';
+export const UPDATED_TEXT = 'updated:';
+export const UPDATED_DEFAULT_TEXT = 'never';
+
+export const LOADING_STATE_TEXT = 'Loading...';
+export const DIARY_PATH = '/diary';
